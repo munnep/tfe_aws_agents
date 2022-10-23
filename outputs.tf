6917a92003417882aa552d0f43b47aa92a9aff39
munnep <patrick.munne@hashicorp.com>
2022-10-23 12:32:09 +0200

    output that does not belong here

--- outputs.tf
+++ outputs.tf
@@ -2,10 +2,6 @@ output "ssh_tfe_server" {
   value = "ssh ubuntu@${var.dns_hostname}.${var.dns_zonename}"
 }
 
-output "ssh_tf_client" {
-  value = "ssh ubuntu@${var.dns_hostname}-client.${var.dns_zonename}"
-}
-
 output "tfe_dashboard" {
   value = "https://${var.dns_hostname}.${var.dns_zonename}:8800"
 }
