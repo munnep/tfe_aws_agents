471f41252d2221cfbd52242dade2a93aa0f6b4b7
munnep <patrick.munne@hashicorp.com>
2022-10-23 11:34:59 +0200

    agent creation true or false

--- agent.tf
+++ agent.tf
@@ -1,4 +1,5 @@
 resource "aws_launch_configuration" "agent" {
+  count                = var.agent_token == "" ? 0 : 1
   name_prefix          = "${var.tag_prefix}-agent"
   image_id             = var.ami
   instance_type        = "t3.small"
@@ -20,5 +21,26 @@ resource "aws_launch_configuration" "agent" {
 
   lifecycle {
     create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "asg_agent" {
+  count                = var.agent_token == "" ? 0 : 1
+  name                 = "${var.tag_prefix}-asg-agent"
+  max_size             = var.asg_max_size
+  min_size             = var.asg_min_size
+  desired_capacity     = var.asg_desired_capacity
+  force_delete         = true
+  launch_configuration = aws_launch_configuration.agent[0].name
+  vpc_zone_identifier  = [aws_subnet.private1.id]
+
+  tag {
+    key                 = "Name"
+    value               = "${var.tag_prefix}-asg-agent"
+    propagate_at_launch = true
+  }
+
+  timeouts {
+    delete = "15m"
   }
 }
